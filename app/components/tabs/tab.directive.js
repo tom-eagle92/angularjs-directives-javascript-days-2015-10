927fc5078cc2973f13a405a8a546d845c654b14e
Sascha Brink <sascha.brink@gmail.com>
2015-10-06 12:03:53 +0200

    Add tabs

--- app/components/tabs/tab.directive.js
+++ app/components/tabs/tab.directive.js
@@ -1,6 +1,15 @@
 angular.module('myApp')
   .directive('tab', function() {
     return {
-
+      scope: {
+        heading: '@'
+      },
+      require: '^tabs',
+      transclude: true,
+      template: '<div class="tab-pane" ng-show="active" ng-transclude></div>',
+      link: function(scope, element, attrs, tabsCtrl) {
+        scope.active = false;
+        tabsCtrl.addTab(scope);
+      }
     }
   });
--- app/components/tabs/tabs.directive.js
+++ app/components/tabs/tabs.directive.js
@@ -1,6 +1,30 @@
 angular.module('myApp')
   .directive('tabs', function() {
     return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      templateUrl: 'components/tabs/tabs.html',
+      bindToController: true,
+      controllerAs: 'tabs',
+      controller: function() {
+        var ctrl = this;
+        ctrl.allTabs = [];
+        ctrl.activeTab = undefined;
 
+        ctrl.selectTab = function(tab) {
+          ctrl.activeTab = tab;
+          angular.forEach(ctrl.allTabs, function(singleTab) {
+            singleTab.active = false;
+          });
+          tab.active = true;
+          console.log('Tab selected', tab)
+        }
+
+        ctrl.addTab = function(tab) {
+          ctrl.allTabs.push(tab);
+          console.log(tab.heading);
+        }
+      }
     }
   });
