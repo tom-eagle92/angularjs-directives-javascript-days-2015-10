4d9303f83b18eb6631dc3c3fdae818c99da2e61d
Sascha Brink <sascha.brink@gmail.com>
2015-10-06 11:07:41 +0200

    Add dummy tabs

---
+++ app/components/tabs/tab.directive.js
@@ -0,0 +1,6 @@
+angular.module('myApp')
+  .directive('tab', function() {
+    return {
+
+    }
+  });
---
+++ app/components/tabs/tabs.directive.js
@@ -0,0 +1,6 @@
+angular.module('myApp')
+  .directive('tabs', function() {
+    return {
+
+    }
+  });
